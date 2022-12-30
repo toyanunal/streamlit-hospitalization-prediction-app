2c2bb3acfaf841d1b819a867ce9f7762bd82db28
Toyan ÜNAL <toyanunal@gmail.com>
2022-12-30 18:05:40 +0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,7 +122,7 @@ def main():
     )
 
     st.title("Hastanede Yatış Süresi Tahminleme")
-    st.write("[![Star](<https://img.shields.io/github/stars/><toyanunal>/<https://github.com/toyanunal/streamlit-hospitalization-prediction-app>.svg?logo=github&style=social)](<https://gitHub.com/><toyanunal>/<https://github.com/toyanunal/streamlit-hospitalization-prediction-app>)")
+    st.write("[![Star](<https://img.shields.io/github/stars/toyanunal>/<https://github.com/toyanunal/streamlit-hospitalization-prediction-app>.svg?logo=github&style=social)](<https://gitHub.com/toyanunal>/<https://github.com/toyanunal/streamlit-hospitalization-prediction-app>)")
     qrcode = Image.open('qrcode.png')
     st.image(qrcode, width=300)
     st.subheader("Lütfen aşağıdaki hasta bilgilerini giriniz:")
