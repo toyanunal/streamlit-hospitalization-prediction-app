db07c7dbe34c6e79a030cb5d699c7b5487bf3a38
Toyan ÜNAL <toyanunal@gmail.com>
2022-12-30 18:27:29 +0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,6 +112,27 @@ def predict(model, input_df):
     prediction = model.predict(input_df)
     return prediction
 
+# Function for background image
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
 # Function for displaying the predictions
 def main():
     st.set_page_config(
@@ -133,28 +154,18 @@ def main():
     # </div>
     # """
 
-    bg_image = """
-    <style>
-    .stApp{{
-    p {
-    background-image: 
-    url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
-    background-attachment: fixed;
-    background-size: cover
-    }}
-    </style>
-    """
+    set_png_as_page_bg('background.png')
 
-    bg_image = '''
-    <style>
-    body {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: cover;
-    }
-    </style>
-    '''
+    # bg_image = '''
+    # <style>
+    # body {
+    # background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    # background-size: cover;
+    # }
+    # </style>
+    # '''
 
-    st.markdown(bg_image, unsafe_allow_html=True)
+    # st.markdown(bg_image, unsafe_allow_html=True)
 
 
 
