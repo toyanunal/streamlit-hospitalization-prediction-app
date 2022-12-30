860268957f7a13d6e2bec97012f1d6f08d8682b6
Toyan ÜNAL <toyanunal@gmail.com>
2022-12-30 13:07:07 +0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ def predict(model, input_df):
 # Function for displaying the predictions
 def main():
     st.title("Hastanede Yatış Süresi Tahminleme")
-    qrcode = Image.open('qrcode.svg')
+    qrcode = Image.open('qrcode')
     st.image(qrcode, caption='QRCode', width=300)
     #st.image("https://www.oreilly.com/library/view/python-data-science/9781491912126/assets/pds_0101.png", width=300)
     st.header("Lütfen aşağıdaki hasta bilgilerini giriniz:")
